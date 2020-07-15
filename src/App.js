73f36d5f77ec9a2e3258325643a406ee4dbfb96b
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2020-07-15 22:27:43 +0700

    update App file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { LoremIpsum } from "lorem-ipsum";
-
-import Home from './components/Home';
-import Play from './components/Play';
-import { ParagraphProvider } from "./context/Paragraph";
-
-import './App.css';
+import Routes from "./routes/Routes";
+import { ParagraphProvider } from "./contexts/Paragraph";
 
 function App() {
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 6
-    },
-    wordsPerSentence: {
-      max: 16,
-      min: 4
-    }
-  });
-
   return (
     <ParagraphProvider>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/play" exact>
-              <Play randomStr={lorem.generateParagraphs(1)}/>
-            </Route>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div className="App">
+        <Routes />
+      </div>
     </ParagraphProvider>
   );
 }
